302adf307e50b607dfe63caee671924a5d521e81
Sanna Luostarinen <sl058088@tite.amiedu-ict.net>
2023-04-13 13:26:36 +0300

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+
+
+import About from './content/About';
+import NotFound from "./content/NotFound";
+import Projects from "./content/Projects";
+import Skills from './content/Skills';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <header>
+          <div id='navContainer' className='box'>
+            <div className='boxHeader'>
+              <h1>Sanna Luostarinen</h1>
+              <div className='boxButtons'>
+                <div className='box'>_</div>
+                <div className='box'>o</div>
+                <div className='box'>x</div>
+              </div>
+            </div>
+            <nav>
+              <Link to="/about" className='box'>About</Link>{' '}
+              <Link to="/projects" className='box'>Projects</Link>{' '}
+              <Link to="/skills" className='box'>Skills</Link>{''}
+            </nav>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path='/skills' element={<Skills />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/content/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About() {
+    return (
+        <div id="aboutContainer">
+            <div className="box">
+                <div className='boxHeader'>
+                    <h2>About me</h2>
+                    <div className='boxButtons'>
+                        <div className='box'>_</div>
+                        <div className='box'>o</div>
+                        <div className='box'>x</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
